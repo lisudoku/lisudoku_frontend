1d27368404be33ec7bf304cb18d152cf4979ad0e
George Marcus <george.marcus922@gmail.com>
2022-11-17 02:11:59 +0200

    Fix footer year

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,6 +1,6 @@
 const AppFooter = () => (
   <footer className="flex flex-col gap-3 pt-2 pb-5 justify-center items-center">
-    <div>© 2022 George Mărcuș</div>
+    <div>© {new Date().getFullYear()} George Mărcuș</div>
     <div className="flex gap-10">
       <a href="/#" className="text-medium">Play</a>
       <a href="/#" className="text-medium">Learn</a>
