8ef239e240bce82be453b692a34ba899d9badffa
George Marcus <george.marcus922@gmail.com>
2022-11-18 23:34:29 +0200

    Move components around

--- src/screens/PlayPage/index.tsx
+++ src/screens/PlayPage/index.tsx
@@ -1,10 +1,14 @@
 import { useParams } from 'react-router-dom'
+import Puzzle from 'src/components/Puzzle'
 
 const PlayPage = () => {
   const { variant } = useParams()
 
   return (
-    <>{variant}</>
+    <>
+      {variant}
+      <Puzzle />
+    </>
   )
 }
 
