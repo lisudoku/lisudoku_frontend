241c2b2fd403b0ba018f21d935e70c88ffdd4a26
George Marcus <george.marcus922@gmail.com>
2023-02-13 16:17:03 +0200

    Add total puzzle count

--- src/screens/AdminPage/GroupCounts.tsx
+++ src/screens/AdminPage/GroupCounts.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import classNames from 'classnames'
+import _ from 'lodash'
 import { fetchGroupCounts } from 'src/utils/apiService'
 import { SudokuDifficulty, SudokuVariant } from 'src/types/sudoku'
 import { useSelector } from 'src/hooks'
@@ -37,14 +38,20 @@ const GroupCounts = () => {
             </tr>
           </thead>
           <tbody className="divide-y">
+            <tr className="h-8 divide-x">
+              <td colSpan={2} className="p-2">Total</td>
+              <td className="flex justify-center p-2 font-bold">
+                {_.sumBy(groupCounts, 'count')}
+              </td>
+            </tr>
             {groupCounts.map(({ variant, difficulty, count }, index) => (
               <tr key={index} className="h-8 divide-x">
                 <td className="p-2">{variant}</td>
                 <td className="p-2">{difficulty}</td>
                 <td className={classNames('flex justify-center p-2 font-bold', {
                   'bg-green-500': count >= 10,
-                  'bg-yellow-200 text-black': count > 0 && count < 10,
-                  'bg-red-400': count === 0,
+                  'bg-yellow-200 text-black': 3 <= count && count < 10,
+                  'bg-red-400': count < 3,
                 })}>{count}</td>
               </tr>
             ))}
