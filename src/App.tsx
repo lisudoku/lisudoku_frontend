4983bbbd835afe2b9e94c7f87c0ff9b930f4213e
George Marcus <george.marcus922@gmail.com>
2023-02-16 00:50:50 +0200

    Fix react-helmet console warning

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { ThemeProvider } from '@material-tailwind/react'
 import { store, persistor } from './store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { HelmetProvider } from 'react-helmet-async'
 import CollectionPage from './screens/CollectionPage'
 import LearnPage from './screens/LearnPage'
 
@@ -30,40 +31,42 @@ if (process.env.NODE_ENV === 'production') {
 
 const App = () => {
   return (
-    <ThemeProvider value={theme}>
-      <div className="min-h-screen md:min-w-fit flex flex-col text-white bg-black">
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                  <Route path="/" element={<Layout padding={false} />}>
-                    <Route path="/" element={<HomePage />} />
-                  </Route>
-                  <Route path="/" element={<Layout />}>
-                    <Route path="/play/:variant/:difficulty" element={<PlayPage />} />
-                    <Route path="login" element={<LoginPage />} />
-                    <Route path="logout" element={<LogoutPage />} />
-                    <Route path="register" element={<RegisterPage />} />
-                    <Route path="p/:id" element={<PuzzlePage />} />
-                    <Route path="tv" element={<TvPage />} />
-                    <Route path="about" element={<AboutPage />} />
-                    <Route path="collections/:id" element={<CollectionPage />} />
-                    <Route path="learn" element={<LearnPage />} />
-                    <Route path="*" element={<PageNotFound />} />
-                  </Route>
-                  <Route path="/" element={<Layout admin />}>
-                    <Route path="/" element={<EnsureAdmin />}>
-                      <Route path="admin/*" element={<AdminPage />} />
+    <HelmetProvider>
+      <ThemeProvider value={theme}>
+        <div className="min-h-screen md:min-w-fit flex flex-col text-white bg-black">
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Routes>
+                    <Route path="/" element={<Layout padding={false} />}>
+                      <Route path="/" element={<HomePage />} />
                     </Route>
-                  </Route>
-                </Routes>
-              </Suspense>
-            </BrowserRouter>
-          </PersistGate>
-        </Provider>
-      </div>
-    </ThemeProvider>
+                    <Route path="/" element={<Layout />}>
+                      <Route path="/play/:variant/:difficulty" element={<PlayPage />} />
+                      <Route path="login" element={<LoginPage />} />
+                      <Route path="logout" element={<LogoutPage />} />
+                      <Route path="register" element={<RegisterPage />} />
+                      <Route path="p/:id" element={<PuzzlePage />} />
+                      <Route path="tv" element={<TvPage />} />
+                      <Route path="about" element={<AboutPage />} />
+                      <Route path="collections/:id" element={<CollectionPage />} />
+                      <Route path="learn" element={<LearnPage />} />
+                      <Route path="*" element={<PageNotFound />} />
+                    </Route>
+                    <Route path="/" element={<Layout admin />}>
+                      <Route path="/" element={<EnsureAdmin />}>
+                        <Route path="admin/*" element={<AdminPage />} />
+                      </Route>
+                    </Route>
+                  </Routes>
+                </Suspense>
+              </BrowserRouter>
+            </PersistGate>
+          </Provider>
+        </div>
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
 
