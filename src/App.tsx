01691cad0a95e0c5f3e3415c5e6ea2e9f8dd9647
George Marcus <george.marcus922@gmail.com>
2023-09-10 20:21:19 +0300

    Refactor AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import { Suspense, lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import EnsureAdmin from './components/EnsureAdmin'
-import HomePage from './screens/HomePage'
-import LoginPage from './screens/LoginPage'
-import LogoutPage from './screens/LogoutPage'
-import RegisterPage from './screens/RegisterPage'
-import PageNotFound from './screens/PageNotFound'
-import PuzzlePage from './screens/PuzzlePage'
-import PlayPage from './screens/PlayPage'
-import TvPage from './screens/TvPage'
-import AboutPage from './screens/AboutPage'
+import { Suspense } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@material-tailwind/react'
 import { store, persistor } from './store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { HelmetProvider } from 'react-helmet-async'
 import HoneybadgerProvider from './components/HoneybadgerProvider'
-import CollectionPage from './screens/CollectionPage'
-import LearnPage from './screens/LearnPage'
-import SolverPage from './screens/SolverPage'
-import ExternalPuzzlePage from './screens/ExternalPuzzlePage'
-import FeedbackPage from './screens/FeedbackPage'
-
-const AdminPage = lazy(() => import('./screens/AdminPage'))
+import AppRoutes from './AppRoutes'
 
 const theme = {}
 
@@ -43,31 +26,7 @@ const App = () => {
               <PersistGate loading={null} persistor={persistor}>
                 <BrowserRouter>
                   <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                      <Route path="/" element={<Layout padding={false} />}>
-                        <Route path="/" element={<HomePage />} />
-                      </Route>
-                      <Route path="/" element={<Layout />}>
-                        <Route path="/play/:variant/:difficulty" element={<PlayPage />} />
-                        <Route path="login" element={<LoginPage />} />
-                        <Route path="logout" element={<LogoutPage />} />
-                        <Route path="register" element={<RegisterPage />} />
-                        <Route path="p/:id" element={<PuzzlePage />} />
-                        <Route path="e" element={<ExternalPuzzlePage />} />
-                        <Route path="tv" element={<TvPage />} />
-                        <Route path="about" element={<AboutPage />} />
-                        <Route path="collections/:id" element={<CollectionPage />} />
-                        <Route path="learn" element={<LearnPage />} />
-                        <Route path="solver" element={<SolverPage />} />
-                        <Route path="feedback" element={<FeedbackPage />} />
-                        <Route path="*" element={<PageNotFound />} />
-                      </Route>
-                      <Route path="/" element={<Layout admin />}>
-                        <Route path="/" element={<EnsureAdmin />}>
-                          <Route path="admin/*" element={<AdminPage />} />
-                        </Route>
-                      </Route>
-                    </Routes>
+                    <AppRoutes />
                   </Suspense>
                 </BrowserRouter>
               </PersistGate>
---
+++ src/AppRoutes.tsx
@@ -0,0 +1,50 @@
+import { lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import HomePage from './screens/HomePage'
+import PlayPage from './screens/PlayPage'
+import LoginPage from './screens/LoginPage'
+import LogoutPage from './screens/LogoutPage'
+import RegisterPage from './screens/RegisterPage'
+import PuzzlePage from './screens/PuzzlePage'
+import ExternalPuzzlePage from './screens/ExternalPuzzlePage'
+import TvPage from './screens/TvPage'
+import AboutPage from './screens/AboutPage'
+import CollectionPage from './screens/CollectionPage'
+import LearnPage from './screens/LearnPage'
+import SolverPage from './screens/SolverPage'
+import FeedbackPage from './screens/FeedbackPage'
+import PageNotFound from './screens/PageNotFound'
+import EnsureAdmin from './components/EnsureAdmin'
+
+const AdminPage = lazy(() => import('./screens/AdminPage'))
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Layout padding={false} />}>
+      <Route path="/" element={<HomePage />} />
+    </Route>
+    <Route path="/" element={<Layout />}>
+      <Route path="/play/:variant/:difficulty" element={<PlayPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="logout" element={<LogoutPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="p/:id" element={<PuzzlePage />} />
+      <Route path="e" element={<ExternalPuzzlePage />} />
+      <Route path="tv" element={<TvPage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="collections/:id" element={<CollectionPage />} />
+      <Route path="learn" element={<LearnPage />} />
+      <Route path="solver" element={<SolverPage />} />
+      <Route path="feedback" element={<FeedbackPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Route>
+    <Route path="/" element={<Layout admin />}>
+      <Route path="/" element={<EnsureAdmin />}>
+        <Route path="admin/*" element={<AdminPage />} />
+      </Route>
+    </Route>
+  </Routes>
+)
+
+export default AppRoutes
