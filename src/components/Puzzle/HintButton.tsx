ad584fb97e1bf7c0d8330268c9471bfebc4351eb
George Marcus <george.marcus922@gmail.com>
2024-01-06 03:02:41 +0200

    Fix tooltip when disabled

--- src/components/Puzzle/HintButton.tsx
+++ src/components/Puzzle/HintButton.tsx
@@ -6,7 +6,7 @@ import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 import { logicalHint } from 'src/utils/wasm'
 import { combineConstraintsWithGrid } from 'src/utils/solver'
 import { changeHintSolution, changePaused } from 'src/reducers/puzzle'
-import { Tooltip } from '@material-tailwind/react'
+import Tooltip from '../Tooltip'
 
 const HINT_DISABLED_TOOLTIP = <div className="w-48">
   You should spend at least 1 minute to figure it out yourself before requesting hints.
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react'
+import { Tooltip as MuiTooltip, TooltipProps as MuiTooltipProps } from '@material-tailwind/react'
+
+interface TooltipProps extends MuiTooltipProps {}
+
+const Tooltip = forwardRef((props: TooltipProps, ref: React.ForwardedRef<HTMLDivElement | null>) => (
+  <>
+    {props.content ? (
+      <MuiTooltip {...props} ref={ref}>
+        {props.children}
+      </MuiTooltip>
+    ) : (
+      props.children
+    )}
+  </>
+))
+
+export default Tooltip
