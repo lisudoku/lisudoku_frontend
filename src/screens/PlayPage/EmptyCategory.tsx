ba98309b71df45932c022538f294fd544ac87052
George Marcus <george.marcus922@gmail.com>
2024-09-17 19:00:41 +0300

    Improve EmptyCategory page

--- src/screens/PlayPage/EmptyCategory.tsx
+++ src/screens/PlayPage/EmptyCategory.tsx
@@ -1,9 +1,17 @@
+import { sample } from 'lodash-es'
 import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 import DifficultySelect from 'src/components/Puzzle/DifficultySelect'
 import VariantSelect from 'src/components/Puzzle/VariantSelect'
 import Typography from 'src/shared/Typography'
 import { SudokuDifficulty, SudokuVariant } from 'src/types/sudoku'
+
+const CONGRATS_MESSAGES = [
+  'What a legend! 💪',
+  'Good job! 🧠',
+  'Heck yeah! 🔥',
+  "Let's go! 🚀",
+]
 
 const EmptyCategory = ({ variant, difficulty }: { variant: SudokuVariant, difficulty: SudokuDifficulty }) => {
   const navigate = useNavigate()
@@ -16,14 +24,25 @@ const EmptyCategory = ({ variant, difficulty }: { variant: SudokuVariant, diffic
   }, [navigate, variant])
 
   return (
-    <div className="flex flex-col gap-5 py-4 px-6">
-      <Typography variant="paragraph">
-        There are no unsolved puzzles here. You can select other variants or difficulties.
+    <div className="flex flex-col w-full items-center gap-5 py-12 px-6">
+      <Typography variant="h2">
+        {sample(CONGRATS_MESSAGES)}
       </Typography>
-      <div className="w-full md:w-32">
+      <div>
+        <Typography variant="paragraph">
+          You solved all of the puzzles in this category.
+        </Typography>
+        <Typography variant="paragraph">
+          Don't worry, we received a notification to craft more puzzles!
+        </Typography>
+        <Typography variant="paragraph">
+          Meanwhile, you can select other variants or difficulties.
+        </Typography>
+      </div>
+      <div className="w-full md:w-40">
         <VariantSelect value={variant} onChange={onVariantChange} />
       </div>
-      <div className="w-full md:w-32">
+      <div className="w-full md:w-40">
         <DifficultySelect value={difficulty} onChange={onDifficultyChange} />
       </div>
     </div>
