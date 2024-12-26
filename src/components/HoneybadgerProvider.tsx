ea6f9eb415c021dafdbcd3206febca27338f8597
George Marcus <george.marcus922@gmail.com>
2024-12-26 17:18:06 +0200

    Exclude some HB alert from headless browsers

--- src/components/HoneybadgerProvider.tsx
+++ src/components/HoneybadgerProvider.tsx
@@ -1,13 +1,20 @@
 import { ReactElement } from 'react'
 import { Honeybadger, HoneybadgerErrorBoundary } from '@honeybadger-io/react'
 
-const config = {
+export const honeybadger = Honeybadger.configure({
   apiKey: import.meta.env.VITE_HONEYBADGER_API_KEY,
   environment: import.meta.env.PROD ? 'production' : 'development',
   revision: 'main',
-}
+})
 
-export const honeybadger = Honeybadger.configure(config)
+honeybadger.beforeNotify((notice) => {
+  if (notice?.name === 'Puzzle import success') {
+    return (
+      !/HeadlessChrome|PhantomJS|Playwright|Puppeteer/.test(navigator.userAgent)
+    )
+  }
+  return true
+})
 
 const HoneybadgerProvider = ({ children }: { children: ReactElement }) => (
   <HoneybadgerErrorBoundary honeybadger={honeybadger}>
