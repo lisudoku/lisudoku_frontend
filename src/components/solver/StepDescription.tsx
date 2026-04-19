1b7ae18fada5e3c2204a29da3a4f26c2a93d486a
George Marcus <george.marcus922@gmail.com>
2026-04-19 20:51:08 +0300

    Make step rule not a link

--- src/components/solver/StepDescription.tsx
+++ src/components/solver/StepDescription.tsx
@@ -2,16 +2,15 @@ import { SolutionStep } from 'lisudoku-solver';
 import { StepRuleDisplay } from 'src/utils/constants';
 import { HintLevel } from 'src/reducers/puzzle';
 import { getBigStepExplanation } from 'src/utils/solver';
-import ExternalLink from '../ExternalLink';
 
 export const StepDescription = (
   { step, hintLevel, gridSize }: { step: SolutionStep, hintLevel: HintLevel, gridSize: number }
 ) => (
   <>
     <b>
-      <ExternalLink url={`/learn#${step.rule}`}>
-        {StepRuleDisplay[step.rule]}
-      </ExternalLink>
+      {/* TODO: Need to find a way to make this an <ExternalLink /> without */}
+      {/* user accidentally clicking on it */}
+      <span className="text-primary font-bold">{StepRuleDisplay[step.rule]}</span>
     </b>
     {hintLevel !== HintLevel.Small && (
       <>
